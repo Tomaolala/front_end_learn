3d29a7c8297467f092d85f48486b272263663887
Yuhang Zi <2694235519@qq.com>
2023-07-04 17:44:36 +0800

    feat: 算法和动画

---
+++ JS/Promise/test.js
@@ -0,0 +1,15 @@
+// const test = new Promise((res)=>{
+//     console.log(2);
+//    res()
+// })
+
+// test.then(res=>{
+//     setTimeout(()=>{
+//         console.log(1);
+//     },10000)
+// }).then(res=>{
+//     setTimeout(()=>{
+//         console.log(3);
+//     },1)
+// })
+
