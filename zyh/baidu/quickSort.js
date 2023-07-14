310d58a751b48bba82c73dfd47d696348c8713db
Yuhang Zi <2694235519@qq.com>
2023-07-14 17:20:49 +0800

    fix: 挽歌

---
+++ zyh/baidu/quickSort.js
@@ -0,0 +1,30 @@
+const arr = [1, 3, 235, 52, 545, 342, 32, 535, 45, 2]
+
+
+function quirckSort(arr, begin, end) {
+    if (begin < end) {
+        let i = begin,
+            j = end
+        let empty = arr[begin]
+        while (i < j) {
+            while (arr[j] > empty && i < j) {
+                j--
+            }
+            arr[i] = arr[j]
+            while (arr[i] <=empty && i < j) {
+                i++
+            }
+            arr[j] = arr[i]
+        }
+        arr[i] = empty
+
+        quirckSort(arr, begin, i - 1)
+        quirckSort(arr, i + 1, end)
+    } else {
+        return
+    }
+
+
+}
+quirckSort(arr, 0, arr.length - 1)
+console.log(arr);
