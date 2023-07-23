c9ba7a5985241b719ddc26ad21fc01a02f58731e
Yuhang Zi <2694235519@qq.com>
2023-07-23 22:24:05 +0800

    fix: 回溯算法

--- JS/test.js
+++ JS/test.js
@@ -1,22 +1,5 @@
-// 闭包 防抖
-const debounce = (fn, t) => {
-    let timer
-    return function () {
-        if (timer) {
-            clearTimeout(timer)
-        }
-        timer = setTimeout(() => {
-            fn()
-        }, t);
-    }
-}
-
-const cl = () => {
-    console.log("kk");
-}
-
-const transform = debounce(cl, 1000)
-// transform()
-
-// 模拟点击
-setInterval(transform,500)
+let path = [2]
+path.push(1)
+let res =[]
+res.push(path)
+console.log(res);
--- 算法eve/全排列.js
+++ 算法eve/全排列.js
@@ -1,20 +1,24 @@
 // 回溯算法
 
-
-
-
-function permute(nums){
-
-    let res  =[]
-
-
-    function bt (used){
-
-        //结束条件
-
-        // used 存储
-        for(let i =0 ;i<nums.lenth;i++){
-
-        }
+function permute(nums) {
+  let res = []; 
+  let used =[ ]
+  let path = [];
+  bt(n,used)
+  function bt(n, used) {
+    //结束条件
+    if(path.length===nums.length){
+        res.push(Array.from(path))
+        return
     }
+    // used 存储
+    for (let i = 0; i < nums.lenth; i++) {
+      if (used[i]) return;
+      path.push(n[i]);
+      used[i] = true;
+      bt(n, used);
+      used[i] = false;
+      path.pop();
+    }
+  }
 }
